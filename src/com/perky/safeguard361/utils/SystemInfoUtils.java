28f23cec2c30df9d46d410ccec4104eb4dd44f6b
ft115637850 <115637850@qq.com>
2015-11-17 07:35:38 +0800

    update

--- src/com/perky/safeguard361/utils/SystemInfoUtils.java
+++ src/com/perky/safeguard361/utils/SystemInfoUtils.java
@@ -1,8 +1,15 @@
 package com.perky.safeguard361.utils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
 import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Context;
 
@@ -16,5 +23,42 @@ public class SystemInfoUtils {
 				return true;
 		}
 		return false;
+	}
+	
+	public static int getProcessCount(Context ctx){
+		ActivityManager am = (ActivityManager) ctx.getSystemService(ctx.ACTIVITY_SERVICE);
+		List<RunningAppProcessInfo> processes = am.getRunningAppProcesses();
+		return processes.size();
+	}
+	
+	public static long getAvailMem(Context ctx){
+		ActivityManager am = (ActivityManager) ctx.getSystemService(ctx.ACTIVITY_SERVICE);
+		ActivityManager.MemoryInfo outInfo = new ActivityManager.MemoryInfo();
+		am.getMemoryInfo(outInfo);
+		long availMem = outInfo.availMem;
+		return availMem;
+	}
+	
+	public static long getTotalMem() {
+		try {
+			FileInputStream fs = new FileInputStream(new File("/proc/meminfo"));
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(fs));
+			String totalInfo = reader.readLine();
+			StringBuilder sb = new StringBuilder();
+			for (char c : totalInfo.toCharArray()) {
+				if (c >= '0' && c <= '9') {
+					sb.append(c);
+				}
+			}
+			return Long.parseLong(sb.toString()) * 1024;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
 	}
 }
