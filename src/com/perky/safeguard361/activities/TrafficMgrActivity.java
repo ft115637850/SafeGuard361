4bb822f9d1fa667ccde61ddfb286461becc1594f
ft115637850 <115637850@qq.com>
2015-11-30 12:39:12 +0800

    add TrafficMgrActivity

---
+++ src/com/perky/safeguard361/activities/TrafficMgrActivity.java
@@ -0,0 +1,13 @@
+package com.perky.safeguard361.activities;
+
+import com.perky.safeguard361.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TrafficMgrActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_trafficmgr);
+	}
+}
