367cba719d4fce3965cd6261ee18f80316fe59b2
ft115637850 <115637850@qq.com>
2015-12-01 22:21:56 +0800

    TrafficInfo complete

--- src/com/perky/safeguard361/activities/TrafficMgrActivity.java
+++ src/com/perky/safeguard361/activities/TrafficMgrActivity.java
@@ -9,10 +9,12 @@ import com.perky.safeguard361.engine.TrafficInfoParser;
 import android.app.Activity;
 import android.net.TrafficStats;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.format.Formatter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -24,6 +26,13 @@ public class TrafficMgrActivity extends Activity {
 	private ProgressBar pb_apps;
 	private ListView lv_apps;
 	private List<TrafficInfo> trafficInfos;
+	private Handler handler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			pb_apps.setVisibility(View.INVISIBLE);
+			lv_apps.setVisibility(View.VISIBLE);
+			lv_apps.setAdapter(new AppManagerAdapter());
+		};
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,19 +48,33 @@ public class TrafficMgrActivity extends Activity {
 		long mobileRx = TrafficStats.getMobileRxBytes();
 		// transfer 发送 上传
 		long mobileTx = TrafficStats.getMobileTxBytes();
-		tv_card.setText(Formatter.formatFileSize(this, mobileRx + mobileTx));
+		tv_card.setText("手机卡："
+				+ Formatter.formatFileSize(this, mobileRx + mobileTx));
 		// 全部的网络信息 wifi + 手机卡
 		long totalRx = TrafficStats.getTotalRxBytes();
 		long totalTx = TrafficStats.getTotalTxBytes();
-		tv_wifi.setText(Formatter.formatFileSize(this, totalRx + totalTx
-				- mobileRx - mobileTx));
+		tv_wifi.setText("Wifi："
+				+ Formatter.formatFileSize(this, totalRx + totalTx - mobileRx
+						- mobileTx));
 		fillData();
 	}
 
 	private void fillData() {
-		trafficInfos = TrafficInfoParser
-				.getTrafficInfos(TrafficMgrActivity.this);
-		lv_apps.setAdapter(new AppManagerAdapter());
+		new Thread() {
+			public void run() {
+				trafficInfos = TrafficInfoParser
+						.getTrafficInfos(TrafficMgrActivity.this);
+				handler.sendEmptyMessage(0);
+			};
+		}.start();
+	}
+
+	private class ViewHolder {
+		public ImageView iv_app_icon;
+		public TextView tv_app_name;
+		public TextView tv_snd;
+		public TextView tv_rcv;
+		public TextView tv_total;
 	}
 
 	private class AppManagerAdapter extends BaseAdapter {
@@ -63,12 +86,36 @@ public class TrafficMgrActivity extends Activity {
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			View view;
+			ViewHolder viewHolder;
 			if (convertView != null && convertView instanceof RelativeLayout) {
 				view = convertView;
+				viewHolder = (ViewHolder) view.getTag();
+
 			} else {
 				view = View.inflate(getApplicationContext(),
 						R.layout.item_trafficmgr, null);
+				viewHolder = new ViewHolder();
+				viewHolder.iv_app_icon = (ImageView) view
+						.findViewById(R.id.iv_app_icon);
+				viewHolder.tv_app_name = (TextView) view
+						.findViewById(R.id.tv_app_name);
+				viewHolder.tv_snd = (TextView) view.findViewById(R.id.tv_snd);
+				viewHolder.tv_rcv = (TextView) view.findViewById(R.id.tv_rcv);
+				viewHolder.tv_total = (TextView) view
+						.findViewById(R.id.tv_total);
+				view.setTag(viewHolder);
 			}
+
+			TrafficInfo trafficInfo = trafficInfos.get(position);
+			viewHolder.iv_app_icon.setImageDrawable(trafficInfo.getIcon());
+			viewHolder.tv_app_name.setText(trafficInfo.getName());
+			viewHolder.tv_rcv.setText("下载："+Formatter.formatFileSize(
+					getApplicationContext(), trafficInfo.getTcp_rcv()));
+			viewHolder.tv_snd.setText("上传："+Formatter.formatFileSize(
+					getApplicationContext(), trafficInfo.getTcp_snd()));
+			viewHolder.tv_total.setText(Formatter.formatFileSize(
+					getApplicationContext(), trafficInfo.getTcp_rcv()
+							+ trafficInfo.getTcp_snd()));
 			return view;
 		}
 
--- src/com/perky/safeguard361/engine/TrafficInfoParser.java
+++ src/com/perky/safeguard361/engine/TrafficInfoParser.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.TrafficStats;
@@ -21,6 +22,11 @@ public class TrafficInfoParser {
 			long snd = TrafficStats.getUidTxBytes(uid);
 			if ((rcv + snd) <= 0)
 				continue;
+
+			int flags = pkg.applicationInfo.flags;
+			if ((ApplicationInfo.FLAG_SYSTEM & flags) != 0) {
+				continue;
+			}
 			TrafficInfo info = new TrafficInfo();
 			info.setTcp_rcv(rcv);
 			info.setTcp_snd(snd);
