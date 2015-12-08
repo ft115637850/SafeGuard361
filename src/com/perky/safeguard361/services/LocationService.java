d1559e93f30de5aa2b17de7a36ec824644ea9e3e
ft115637850 <115637850@qq.com>
2015-12-08 17:53:18 +0800

    add BaiduLBS_Android.jar

--- src/com/perky/safeguard361/services/LocationService.java
+++ src/com/perky/safeguard361/services/LocationService.java
@@ -1,23 +1,20 @@
 package com.perky.safeguard361.services;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Bundle;
 import android.os.IBinder;
 import android.telephony.SmsManager;
-import android.text.TextUtils;
-import android.util.Log;
-
-import android.location.LocationListener;
 
 public class LocationService extends Service {
-	private LocationManager lm;
-	private MyListener listener;
+	// private LocationManager lm;
+	// private MyListener listener;
+	private LocationClient mLocationClient;
+	private MyLocationListenner myListener;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -27,28 +24,60 @@ public class LocationService extends Service {
 
 	@Override
 	public void onCreate() {
-		lm = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-		Criteria crt = new Criteria();
-		crt.setAccuracy(Criteria.ACCURACY_FINE);
-		crt.setCostAllowed(true);
-		String provider = lm.getBestProvider(crt, true);
-		Log.i("provider", "?" + provider);
-		listener = new MyListener();
-		if (!TextUtils.isEmpty(provider)) {
-			lm.requestLocationUpdates(provider, 0, 0, listener);
-		}
-		else{
-			lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
-		}
+		/*
+		 * lm = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+		 * Criteria crt = new Criteria();
+		 * crt.setAccuracy(Criteria.ACCURACY_FINE); crt.setCostAllowed(true);
+		 * String provider = lm.getBestProvider(crt, true); Log.i("provider",
+		 * "?" + provider); listener = new MyListener(); if
+		 * (!TextUtils.isEmpty(provider)) { lm.requestLocationUpdates(provider,
+		 * 0, 0, listener); } else{
+		 * lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+		 * listener); }
+		 */
+		mLocationClient = new LocationClient(getApplicationContext()); // 声明LocationClient类
+		mLocationClient.registerLocationListener(myListener); // 注册监听函数
 		super.onCreate();
 	}
 
-	private class MyListener implements LocationListener {
+	/*
+	 * private class MyListener implements LocationListener {
+	 * 
+	 * @Override public void onLocationChanged(Location location) {
+	 * StringBuilder sb = new StringBuilder(); sb.append("getAccuracy:" +
+	 * location.getAccuracy() + "\n"); sb.append("Altitude:" +
+	 * location.getAltitude() + "\n"); sb.append("Latitude:" +
+	 * location.getLatitude() + "\n"); sb.append("Longitude:" +
+	 * location.getLongitude() + "\n"); SharedPreferences sp =
+	 * getSharedPreferences("config", Context.MODE_PRIVATE); String safenumber =
+	 * sp.getString("safenumber", "");
+	 * SmsManager.getDefault().sendTextMessage(safenumber, null, sb.toString(),
+	 * null, null); stopSelf(); }
+	 * 
+	 * @Override public void onStatusChanged(String provider, int status, Bundle
+	 * extras) { // TODO Auto-generated method stub
+	 * 
+	 * }
+	 * 
+	 * @Override public void onProviderEnabled(String provider) { // TODO
+	 * Auto-generated method stub
+	 * 
+	 * }
+	 * 
+	 * @Override public void onProviderDisabled(String provider) { // TODO
+	 * Auto-generated method stub
+	 * 
+	 * } }
+	 */
+
+	/**
+	 * 定位SDK监听函数
+	 */
+	public class MyLocationListenner implements BDLocationListener {
 
 		@Override
-		public void onLocationChanged(Location location) {
+		public void onReceiveLocation(BDLocation location) {
 			StringBuilder sb = new StringBuilder();
-			sb.append("getAccuracy:" + location.getAccuracy() + "\n");
 			sb.append("Altitude:" + location.getAltitude() + "\n");
 			sb.append("Latitude:" + location.getLatitude() + "\n");
 			sb.append("Longitude:" + location.getLongitude() + "\n");
@@ -60,29 +89,18 @@ public class LocationService extends Service {
 			stopSelf();
 		}
 
-		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onProviderEnabled(String provider) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void onProviderDisabled(String provider) {
-			// TODO Auto-generated method stub
-
+		public void onReceivePoi(BDLocation poiLocation) {
 		}
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		lm.removeUpdates(listener);
-		listener = null;
+		// lm.removeUpdates(listener);
+		// listener = null;
+		// 退出时销毁定位
+		mLocationClient.unRegisterLocationListener(myListener);
+		mLocationClient.stop();
+		mLocationClient = null;
 	}
 }
